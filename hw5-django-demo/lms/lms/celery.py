dfe5dc163fc504e87e11ef5afc18e4ea2e1778e0
Artem Hodunov <hodunov.art@gmail.com>
2021-05-08 15:28:01 +0300

    REST API for Groups, Students and Teachers

--- hw5-django-demo/lms/lms/celery.py
+++ hw5-django-demo/lms/lms/celery.py
@@ -1,6 +1,8 @@
 import os
-
 from celery import Celery
+
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.settings')
 
@@ -14,3 +16,10 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
+@app.task
+def update_token():
+    for user in User.objects.all():
+        token = Token.objects.filter(user=user)
+        new_key = token[0].generate_key()
+        token.update(key=new_key)
--- hw5-django-demo/lms/lms/settings.py
+++ hw5-django-demo/lms/lms/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,8 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Local Apps
     'core',
 
+    # Third-Party Apps
+    'rest_framework',
+    'rest_framework.authtoken',
     'debug_toolbar',
     'django_forms_bootstrap',
 ]
@@ -78,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lms.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -88,7 +89,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -108,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -122,7 +121,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -135,7 +133,7 @@ INTERNAL_IPS = [
 ]
 
 # https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
-DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 ADMIN_EMAIL = "admin@example.com"
 SUPPORT_EMAIL_FROM = "support@example.com"
@@ -145,5 +143,18 @@ CELERY_BEAT_SCHEDULE = {
     'my-first-regular-task': {
         'task': 'core.tasks.test_regular_task',
         'schedule': crontab(minute=0, hour=0)
-    }
+    },
+    'new-token-every-midnight': {
+        'task': 'core.tasks.generate_new_token',
+        'schedule': crontab(hour=0, minute=0),
+    },
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
