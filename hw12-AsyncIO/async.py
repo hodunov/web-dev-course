9a1ff18998d0696071096de49b62fe9af357bee8
Artem Hodunov <hodunov.art@gmail.com>
2021-05-08 16:48:56 +0300

    Asynchronous version of file reading

--- hw12-AsyncIO/async.py
+++ hw12-AsyncIO/async.py
@@ -6,8 +6,7 @@ from file_generator import FILE_FOLDER_NAME, CWD, AMOUNT_OF_FILES
 
 files_dir = os.path.join(CWD, FILE_FOLDER_NAME)
 
-all_res = []
-start = datetime.now()
+RESULT = []
 
 
 async def line_reader(afp):
@@ -17,26 +16,26 @@ async def line_reader(afp):
         second_line = lines[1].strip()
         num_1, num_2 = [float(word) for word in second_line.split()]
         if first_line == '1':
-            all_res.append(num_1 + num_2)
+            RESULT.append(num_1 + num_2)
         if first_line == '2':
-            all_res.append(num_1 * num_2)
+            RESULT.append(num_1 * num_2)
         if first_line == '3':
-            all_res.append(num_1 ** 2 + num_2 ** 2)
+            RESULT.append(num_1 ** 2 + num_2 ** 2)
 
 
 async def main():
     tasks = []
     for number in range(1, AMOUNT_OF_FILES + 1):
         tasks.append(line_reader(os.path.join(files_dir, f"in_{number}.dat")))
-    # for task in tasks:
-    #     await task
     await asyncio.gather(*tasks)
 
-# asyncio.run(main())
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-
-print(sum(map(float, all_res)))
-end = datetime.now()
-time_taken = end - start
-print('Time: ', time_taken)
+if __name__ == '__main__':
+    start = datetime.now()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    sum_result = sum(map(float, RESULT))
+    with open('out.dat', 'w') as out_file:
+        out_file.write(str(sum_result))
+    end = datetime.now()
+    time_taken = end - start
+    print('Time: ', time_taken)
--- hw12-AsyncIO/sync_version.py
+++ hw12-AsyncIO/sync_version.py
@@ -4,8 +4,7 @@ from file_generator import FILE_FOLDER_NAME, CWD, AMOUNT_OF_FILES
 
 files_dir = os.path.join(CWD, FILE_FOLDER_NAME)
 
-all_res = []
-start = datetime.now()
+RESULT = []
 
 
 def line_reader(afp):
@@ -15,19 +14,21 @@ def line_reader(afp):
         second_line = afp.readline()
         num_1, num_2 = [float(word) for word in second_line.split()]
         if first_line == '1':
-            all_res.append(num_1 + num_2)
+            RESULT.append(num_1 + num_2)
         if first_line == '2':
-            all_res.append(num_1 * num_2)
+            RESULT.append(num_1 * num_2)
         if first_line == '3':
-            all_res.append(num_1 ** 2 + num_2 ** 2)
+            RESULT.append(num_1 ** 2 + num_2 ** 2)
 
 
 for number in range(1, AMOUNT_OF_FILES + 1):
     line_reader(os.path.join(files_dir, f"in_{number}.dat"))
 
-print(sum(map(float, all_res)))
-
-end = datetime.now()
-time_taken = end - start
-
-print('Time: ', time_taken)
+if __name__ == '__main__':
+    start = datetime.now()
+    sum_result = sum(map(float, RESULT))
+    with open('out.dat', 'w') as out_file:
+        out_file.write(str(sum_result))
+    end = datetime.now()
+    time_taken = end - start
+    print('Time: ', time_taken)
